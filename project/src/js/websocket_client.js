5338c2dc8a2c151f14bbd02909b7b4e251209907
andris.akacenoks <v-anakac@microsoft.com>
2018-04-25 21:35:45 +0300

    Test

--- project/src/js/websocket_client.js
+++ project/src/js/websocket_client.js
@@ -67,5 +67,5 @@ function showRT32(){
     document.getElementById('acu-params').style.visibility='hidden';
     document.getElementById('show-status').style.visibility='hidden';
     createWebSocket(2);
-    showToast("RXC is not avaliable for RT32");
+    //showToast("RXC is not avaliable for RT32");
 }
