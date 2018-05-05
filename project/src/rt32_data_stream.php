70dce43ef487830c354ad43a32c155784591c15d
andris.akacenoks <v-anakac@microsoft.com>
2018-05-05 13:44:36 +0300

    db

--- project/src/rt32_data_stream.php
+++ project/src/rt32_data_stream.php
@@ -9,6 +9,15 @@ header("Content-Type: text/event-stream");
 header("Cache-Control: no-cache");
 header("Access-Control-Allow-Origin: *");
 
+//database
+define('DB_HOST', '127.0.0.1');
+define('DB_USERNAME', 'oper');
+define('DB_PASSWORD', 'parole');
+define('DB_NAME', 'irbene');
+
+//get connection
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
 // start stream
 while(true){
 
@@ -17,13 +26,26 @@ while(true){
     }
 
     else{
-        $fh = fopen('../../../../clients/rt32.json','r');
-        while ($line = fgets($fh)) {
-          echo "data: {$line}\n\n";
-          ob_flush();
-          flush();
+        $query = "SELECT * FROM rt32 LIMIT 1 ORDER BY Measurement_ID ASC";
+    
+        //execute query
+        $result = $mysqli->query($query);
+    
+        //loop through the returned data
+        $data = array();
+        foreach ($result as $row) {
+            $data[] = $row;
         }
-        fclose($fh);
+    
+        //free memory associated with result
+        $result->close();
+    
+        //close connection
+        $mysqli->close();
+    
+        //now print the data
+        print json_encode($data);
+
     }
     sleep(1);
 }
