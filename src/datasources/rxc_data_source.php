eef7f58f9cfea6c414978d3d83f915f5cc935288
andris.akacenoks <andris.akacenoks@testdevlab.com>
2018-05-22 21:13:01 +0300

    Added data source

--- src/datasources/rxc_data_source.php
+++ src/datasources/rxc_data_source.php
@@ -3,42 +3,48 @@
 	error_reporting(E_ALL);
 	header('Access-Control-Allow-Origin: *');
 	header('Content-Type: application/json');
+    print json_encode("Before config");
 
 	$config = parse_ini_file('../../../clients/config.ini'); 
     $mysqli = mysqli_connect('127.0.0.1',$config['username'],$config['password'],$config['dbname']);
-	
+    print json_encode("after config");
+
 	if(!$mysqli){
 		die("Connection failed: " . $mysqli->error);
 	}
 
-	$step = 1;
 
 	if(isset($_GET['interval'])){
 		if(($_GET['interval'] === '24h') || ($_GET['interval'] === 'undefined') ){
 			$yesterdayDateSQL = date('Y-m-d H:i:s', strtotime("-7 day 4 hour"));
-			$todayDateSQL = date('Y-m-d H:i:s', strtotime("-3 hour"));
+            $todayDateSQL = date('Y-m-d H:i:s', strtotime("-3 hour"));
+            print json_encode("hey");
         }
     }
 
+    print json_encode("before query");
 
 	$query = "SELECT Data FROM rt16 WHERE (measurement_time >= '{$yesterdayDateSQL}' AND measurement_time <= '{$todayDateSQL}')";
 
-	//execute query
+	// //execute query
 	$result = $mysqli->query($query);
+    print json_encode("after query");
 
-	//loop through the returned data
+
+	// //loop through the returned data
 	$data = array();
 	foreach ($result as $row) {
 		$data[] = $row;
 	}
 
-	//free memory associated with result
+	// //free memory associated with result
 	$result->close();
 
-	//close connection
+	// //close connection
 	$mysqli->close();
 
 	//now print the data
     print json_encode($data);
+    print json_encode("test");
 
 ?>
